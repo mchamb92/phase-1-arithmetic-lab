4feb117aa2718f4d36ad26f77acf57d00efc1313
Mustafa <mchambqa@gmail.com>
2021-08-10 22:02:14 -0400

    commit tree 3494f69cfffb3dde532dca8a720f0887848a15bc

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 
 // Write your code here
+const num1 = 31;
+const num2 = 2;
+const multiply  = num1 * num2;
+
+
+let random = Math.floor(Math.random() * 10) + 1;
+
+
+ const num3 = 4;
+ const num4 = 8;
+
+ const mod = num3 % num4;
+
+const max = Math.max(10,8,15,20,9,12,17,);
